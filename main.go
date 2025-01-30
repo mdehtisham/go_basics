2be7f9ec806e7df8930b77a25323a91ef686f836
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-01-30 23:38:01 +0530

    values in go

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
-	test()
+	valuesInGo()
 }
 
-func test() {
-	fmt.Println("Testing live reload!")
+func valuesInGo() {
+	fmt.Println("valuesInGoing live reload!")
+
+	fmt.Println("go"+"lang", 3+7, "ji")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
