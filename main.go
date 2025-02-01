f908d1ad6fde50db9014d27f7900595ca7298946
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-02-01 09:36:43 +0530

    storing different value types in map

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ func main() {
 	// fmt.Println("hello world")
 	// valuesInGo()
 	// forLoop()
-	mapExample()
+	// mapExample()
+	createMixedMap()
 }
 
 func valuesInGo() {
@@ -36,13 +37,52 @@ func mapExample() {
 	identityMap["Student"] = "true"
 	fmt.Println(identityMap)
 
+	// Accessing elements
+	fmt.Println("identityMap[\"name\"]:", identityMap["name"])
+
 	// // Declare a map using map literal
 	m2 := map[string]int{
 		"apple":  5,
 		"orange": 10,
 	}
 	fmt.Println(m2)
+	// Deleting elements
 	delete(m2, "apple")
 	fmt.Println(m2)
 
+	// checking if a key exists
+	value, exist := m2["apple"]
+	if exist {
+		fmt.Println(value)
+	} else {
+		fmt.Println("Value not found")
+	}
+
+	// iterating over a map
+	for key, value := range identityMap {
+		fmt.Printf("key is %s and value is %s \n", key, value)
+	}
+
+}
+
+func createMixedMap() {
+	// Declare a map with string keys and interface{} values
+	var mixedMap = make(map[string]interface{})
+
+	// Adding elements of different types
+	mixedMap["name"] = "John"
+	mixedMap["age"] = 30
+	mixedMap["isStudent"] = true
+	mixedMap["grades"] = []int{90, 85, 88}
+
+	// Accessing and printing elements
+	// fmt.Println("Name:", mixedMap["name"])
+	// fmt.Println("Age:", mixedMap["age"])
+	// fmt.Println("Is Student:", mixedMap["isStudent"])
+	// fmt.Println("Grades:", mixedMap["grades"])
+
+	// Iterating over the map
+	for key, value := range mixedMap {
+		fmt.Printf("Key: %s, Value: %v and type is %T\n", key, value, value)
+	}
 }
