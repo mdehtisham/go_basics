e0ed6a4878ba8d9d8596582e5a52057f3fc62287
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-02-05 23:32:33 +0530

    imports details

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	mapUtil "main/basics"
 )
 
+/*
+Imports
+This code groups the imports into a parenthesized, "factored" import statement.
+
+You can also write multiple import statements, like:
+
+import "fmt"
+import "math"
+But it is good style to use the factored import statement.
+*/
+
 func main() {
 	// fmt.Println("hello world")
 	// valuesInGo()
