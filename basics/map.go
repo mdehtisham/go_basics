b5447ac41eace696ac190477daa7ffd338104727
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-02-06 22:25:59 +0530

    exported names details

--- basics/map.go
+++ basics/map.go
@@ -12,6 +12,15 @@ package mapUtil
 
 import "fmt"
 
+/*
+In Go, a name is exported if it begins with a capital letter. For example, Pizza is an exported name, as is Pi, which is exported from the math package.
+
+pizza and pi do not start with a capital letter, so they are not exported.
+
+When importing a package, you can refer only to its exported names. Any "unexported" names are not accessible from outside the package.
+
+*/
+
 func MapExample() {
 	// Declare a map using make
 	var identityMap = make(map[string]string)
