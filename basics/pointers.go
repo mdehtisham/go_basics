d210f51ae534ac982857af1e4577521be6957583
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-02-23 23:12:35 +0530

    struct pointers in go

--- basics/pointers.go
+++ basics/pointers.go
@@ -56,3 +56,23 @@ func structPoiterExample() {
 	p.X = 1e9
 	fmt.Println(v)
 }
+
+/*
+Struct Literals
+A struct literal denotes a newly allocated struct value by listing the values of its fields.
+
+You can list just a subset of fields by using the Name: syntax. (And the order of named fields is irrelevant.)
+
+The special prefix & returns a pointer to the struct value.
+
+*/
+
+func structLiteralExample() {
+	var (
+		v1 = Vertex{1, 2}  // has type Vertex
+		v2 = Vertex{X: 1}  // Y:0 is implicit
+		v3 = Vertex{}      // X:0 and Y:0
+		p  = &Vertex{1, 2} // has type *Vertex
+	)
+	fmt.Println(v1, p, v2, v3)
+}
