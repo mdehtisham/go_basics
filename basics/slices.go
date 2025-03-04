bc65c628bc5793fa69a112461364747fc1755324
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-03-04 07:53:46 +0530

    slice continue

--- basics/slices.go
+++ basics/slices.go
@@ -159,3 +159,17 @@ func sliceLength() {
 func printSlice(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
+
+/*
+Nil slices
+The zero value of a slice is nil.
+
+A nil slice has a length and capacity of 0 and has no underlying array.
+*/
+func nilSlice() {
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
+}
