a27cb437f40df1d1d569aef32313eeb791c0cc61
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-03-18 21:37:41 +0530

    slice continued

--- basics/slices.go
+++ basics/slices.go
@@ -263,3 +263,24 @@ func appendSlice() {
 	s = append(s, 2, 3, 4)
 	printSlice(s)
 }
+
+/*
+Exercise: Slices
+Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers. When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
+
+The choice of image is up to you. Interesting functions include (x+y)/2, x*y, and x^y.
+
+(You need to use a loop to allocate each []uint8 inside the [][]uint8.)
+
+(Use uint8(intValue) to convert between types.)
+*/
+func Pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dy)
+	for i := range pic {
+		pic[i] = make([]uint8, dx)
+		for j := range pic[i] {
+			pic[i][j] = uint8((i + j) / 2)
+		}
+	}
+	return pic
+}
