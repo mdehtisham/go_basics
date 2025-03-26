fa52a70fb7cfdcea7f5005053e96b0f34f674fba
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-03-26 22:14:57 +0530

    mutating maps

--- basics/maps.go
+++ basics/maps.go
@@ -80,3 +80,18 @@ Note: If elem or ok have not yet been declared you could use a short declaration
 
 elem, ok := m[key]
 */
+func mutatingMaps() {
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+}
