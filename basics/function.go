599edc32894078bb3ad25341c97b329ffe92f6dc
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-04-01 22:51:12 +0530

    function closure

--- basics/function.go
+++ basics/function.go
@@ -32,3 +32,20 @@ Go functions may be closures. A closure is a function value that references vari
 
 For example, the adder function returns a closure. Each closure is bound to its own sum variable.
 */
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func adderMain() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
