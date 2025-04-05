2254c2ea358cf43fdc770ea9ac53f0f67812ecb2
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-04-05 14:58:41 +0530

    methods example

--- basics/methods.go
+++ basics/methods.go
@@ -9,3 +9,23 @@ The receiver appears in its own argument list between the func keyword and the m
 In this example, the Abs method has a receiver of type Vertex named v.
 */
 package mapUtil
+
+import (
+	"fmt"
+	"math"
+)
+
+type MethodsVertex struct {
+	X, Y float64
+}
+
+func (v MethodsVertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func useAbs() {
+	v := MethodsVertex{3, 4}
+	// Struct type method
+	abs := v.Abs()
+	fmt.Println(abs)
+}
