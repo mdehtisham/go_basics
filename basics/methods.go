3d96c0133aa5210afd22a6e1b4206b1d0482d4aa
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-04-12 12:34:33 +0530

    methods continue

--- basics/methods.go
+++ basics/methods.go
@@ -44,3 +44,17 @@ In this example we see a numeric type MyFloat with an Abs method.
 You can only declare a method with a receiver whose type is defined in the same package as the method. You cannot declare a method with a receiver whose type is defined in another package (which includes the built-in types such as int).
 */
 //
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func methodOnNonStruct() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
