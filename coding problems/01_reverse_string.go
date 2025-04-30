2146ab9e064a60de48cb6e10e9e086c15d9519af
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-04-30 08:43:27 +0530

    reverse string

---
+++ coding problems/01_reverse_string.go
@@ -0,0 +1,13 @@
+/*
+Problem 1: Reverse a String
+Concept: Strings are immutable in Go. Use slices of rune to handle Unicode characters.
+
+Problem Statement:
+Write a function to reverse a given string.
+
+Sample Input: "hello"
+Sample Output: "olleh"
+*/
+package main
+
+// ReverseString reverses a given string.
