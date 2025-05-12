69b3c3a85a76e47f14b0dd11490c7fa62980e1de
Md Ehtisham Farooqui <ehtisham@Mds-MacBook-Pro.local>
2025-05-12 11:33:34 +0530

    anagram

---
+++ coding problems/04_anagram.go
@@ -0,0 +1,9 @@
+// Problem 4: Check if Two Strings Are Anagrams
+// Concept: Using slices and sorting with sort.Slice.
+
+// Problem Statement:
+// Write a function to check if two strings are anagrams (contain the same characters in any order).
+
+// Sample Input 1: "listen", "silent" → Output: true
+// Sample Input 2: "hello", "world" → Output: false
+
